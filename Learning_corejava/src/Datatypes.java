fdd34deeaea588c3b9b928924d9563bf8fd0c811
chintan <chintan.tops@gmail.com>
2023-08-19 18:06:56 +0530

    datatypes

---
+++ Learning_corejava/src/Datatypes.java
@@ -0,0 +1,31 @@
+
+public class Datatypes {
+	public static void main(String[] args) {
+		
+		
+		byte b = -128;
+		
+		short s = 32767;
+		
+		int i = 56565656;
+		
+		long l = 44655556;
+		
+		float f = 253.55466464646464f;
+		double d = 225.664445461164646;
+		
+		System.out.println(f);
+		System.out.println(d);
+		
+		char ch = 'a';
+		
+		boolean bool = true;
+		
+		
+		
+		
+		
+		
+		
+	}
+}
